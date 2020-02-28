638c0fe6f5f813cd9e8db6b0d955277fd5fa4818
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-28 22:39:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,7 +62,18 @@ end
   end
   
   def reduce_to_any_true(source_array)
-    
+    i=0 
+    while i<source_array.length do 
+      element=source_array[i]
+      if element
+       return true 
+      end 
+      i += 1 
+    end 
+    return false 
+  end 
+  
+        
       
  
  
